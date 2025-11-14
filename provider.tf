61f9857305bc84bcc759c5fa5dfe04b8121cdef0
Ajay Spak <ajayspakcommcom>
2025-11-14 15:43:38 +0530

    changes

--- provider.tf
+++ provider.tf
@@ -1,23 +1,3 @@
-# terraform {
-#   required_version = ">= 1.5.0"
-
-#   required_providers {
-#     aws = {
-#       source  = "hashicorp/aws"
-#       version = "~> 5.0"
-#     }
-#   }
-
-#   backend "s3" {
-#     bucket         = "tanvora-terraform-state-001"
-#     key            = "infra/terraform.tfstate"
-#     region         = "ap-south-1"
-#     dynamodb_table = "tanvora-terraform-lock"
-#     encrypt        = true
-#   }
-
-# }
-
 # Main AWS provider (your primary region)
 provider "aws" {
   region = var.aws_region
