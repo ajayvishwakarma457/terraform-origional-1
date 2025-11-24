dfa0723fa2318f0e74be39bc210faff64e3e0c25
Ajay Spak <ajayspakcommcom>
2025-11-24 17:25:38 +0530

    changes

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "vpc" {
-  source = "./modules/vpc"
+  source = "./modules/networking/vpc"
 
   project_name         = var.project_name
   aws_region           = var.aws_region
@@ -11,41 +11,41 @@ module "vpc" {
 }
 
 
-module "route53" {
-  source       = "./modules/route53"
-  project_name = var.project_name
-  domain_name  = var.domain_name
-  common_tags  = var.common_tags
+# module "route53" {
+#   source       = "./modules/networking/route53"
+#   project_name = var.project_name
+#   domain_name  = var.domain_name
+#   common_tags  = var.common_tags
 
-  a_records = [
-    {
-      name    = var.domain_name
-      ttl     = 300
-      records = ["1.2.3.4"]
-    }
-  ]
+#   a_records = [
+#     {
+#       name    = var.domain_name
+#       ttl     = 300
+#       records = ["1.2.3.4"]
+#     }
+#   ]
 
-  cname_records = [
-    {
-      name  = "www"
-      ttl   = 300
-      value = var.domain_name
-    }
-  ]
+#   cname_records = [
+#     {
+#       name  = "www"
+#       ttl   = 300
+#       value = var.domain_name
+#     }
+#   ]
 
-  txt_records = [
-    {
-      name    = "_verify"
-      ttl     = 300
-      records = ["some-verification-token"]
-    }
-  ]
+#   txt_records = [
+#     {
+#       name    = "_verify"
+#       ttl     = 300
+#       records = ["some-verification-token"]
+#     }
+#   ]
 
-  mx_records = [
-    {
-      name    = var.domain_name
-      ttl     = 300
-      records = ["10 mail.${var.domain_name}"]
-    }
-  ]
-}
+#   mx_records = [
+#     {
+#       name    = var.domain_name
+#       ttl     = 300
+#       records = ["10 mail.${var.domain_name}"]
+#     }
+#   ]
+# }
